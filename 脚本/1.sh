159e7925eeba5c7326ab325c4b6ba80c9ec4328f
dclangx <dc_langx@126.com>
2018-07-02 15:53:34 +0800

    修正

--- 脚本/1.sh
+++ 脚本/1.sh
@@ -6,10 +6,11 @@ else
     cd $1
     mkdir css js
     touch index.html css/style.css js/main.js
+    echo "<!DOCTYPE>" > index.html
+    echo "<title>Hello</title>" >> index.html
+    echo "<h1>Hi</h1>" >> index.html
+    echo "h1{color: red;}" > css/style.css
+    echo "var string = \"Hello World\";alert(string)" > js/main.js
     exit
 fi
-echo "<!DOCTYPE>" > index.html
-echo "<title>Hello</title>" >> index.html
-echo "<h1>Hi</h1>" >> index.html
-echo "h1{color: red;}" > css/style.css
-echo "var string = \"Hello World\";alert(string)" > js/main.js
+
