c5deac87e01b07a23712fa2bc013bbe10711b8d7
dclangx <dc_langx@126.com>
2018-08-17 14:58:27 +0800

    添加loading

---
+++ 简历/flex重写/js/1.js
@@ -0,0 +1,13 @@
+portfolio1.onclick = function () {
+    portfolioBar.className = 'bar state-1'
+}
+portfolio2.onclick = function () {
+    portfolioBar.className = 'bar state-2'
+}
+portfolio3.onclick = function () {
+    portfolioBar.className = 'bar state-3'
+}
+
+
+var loading = document.querySelector('.loading');
+loading.classList.add('none');
