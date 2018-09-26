fa6c0752d14196a5496debcb5f68edec82c29d30
dclangx <dc_langx@126.com>
2018-09-26 13:06:19 +0800

    加入方案2

--- 浮层/main.js
+++ 浮层/main.js
@@ -1,12 +1,27 @@
 var clickBtn = document.getElementById('clickme');
 var popover = document.getElementById('popover');
 var wrapper = document.getElementById('wrapper');
-clickBtn.addEventListener('click', () => {
-    popover.style.display = 'inline-block';
-});
+// clickBtn.addEventListener('click', () => {
+//     popover.style.display = 'inline-block';
+// });
+// document.addEventListener('click', () => {
+//     popover.style.display = 'none';
+// });
+// wrapper.addEventListener('click', (e) => {
+//     e.stopPropagation();
+// })
+
+// planB
+$(clickme).on('click', function () {
+    $(popover).show();
+    console.log('show');
+    setTimeout(function () {
+        $(document).one('click', function () {
+            console.log('hide');
+            $(popover).hide()
+        })
+    }, 0)
+})
 wrapper.addEventListener('click', (e) => {
     e.stopPropagation();
-})
-document.addEventListener('click', () => {
-    popover.style.display = 'none';
 })
