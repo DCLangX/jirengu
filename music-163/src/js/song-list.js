5895434de82eb5d0417e69dfa85ff2ce236f4582
dclangx <dc_langx@126.com>
2018-12-21 16:02:11 +0800

    模块化

---
+++ music-163/src/js/song-list.js
@@ -0,0 +1,30 @@
+{
+    let view = {
+        el: '#song_main',
+        template: `
+        <ul class="song_list">
+            <li>啦啦啦</li>
+            <li>啦啦啦</li>
+            <li>啦啦啦</li>
+            <li>啦啦啦</li>
+            <li>啦啦啦</li>
+            <li>啦啦啦</li>
+            <li>啦啦啦</li>
+        </ul>
+        `,
+        render: function (data) {
+            $(this.el).html(this.template)
+        }
+    }
+    let model = {
+
+    }
+    let controller = {
+        init: function (view, model) {
+            this.view = view
+            this.model = model
+            this.view.render(this.model.data)
+        }
+    }
+    controller.init(view, model)
+}
