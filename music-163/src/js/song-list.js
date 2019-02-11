6b499f263ad28988e930c2949a79bcd0aff75694
dclangx <dc_langx@126.com>
2019-02-11 14:29:38 +0800

    添加lencloud查询

--- music-163/src/js/song-list.js
+++ music-163/src/js/song-list.js
@@ -7,23 +7,37 @@
         `,
         render: function (data) {
             $(this.el).html(this.template)
-            let {songs} =data
-            let liList = songs.map((song)=>{
+            let {
+                songs
+            } = data
+            let liList = songs.map((song) => {
                 let li = $('<li></li>').text(song.name)
                 return li
             })
             $(this.el).find('ul').empty()
-            liList.map((domLi)=>{
+            liList.map((domLi) => {
                 $(this.el).find('ul').append(domLi)
             })
         },
-        clearActive:function(){
+        clearActive: function () {
             $(this.el).find('.active').removeClass('active')
         }
     }
     let model = {
-        data:{
-            songs:[]
+        data: {
+            songs: []
+        },
+        find: function () {
+            var query = new AV.Query('Song');
+            return query.find().then((songs) => {
+                this.data.songs = songs.map((element) => {
+                    return {
+                        id: element.id,
+                        ...element.attributes
+                    }
+                })
+                return songs
+            })
         }
     }
     let controller = {
@@ -31,11 +45,14 @@
             this.view = view
             this.model = model
             this.view.render(this.model.data)
-            window.eventHub.on('upload',()=>{
+            window.eventHub.on('upload', () => {
                 this.view.clearActive()
             })
-            window.eventHub.on('create',(songData)=>{
+            window.eventHub.on('create', (songData) => {
                 this.model.data.songs.push(songData)
+                this.view.render(this.model.data)
+            })
+            this.model.find().then(()=>{
                 this.view.render(this.model.data)
             })
         }
