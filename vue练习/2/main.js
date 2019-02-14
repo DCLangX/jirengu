f18e8a2189eae4e8a9b77a79264f60bfa75eea49
爱吃薯片的内存条 <dc_langx@126.com>
2019-02-14 23:55:40 +0800

    vue侦听器和计算属性练习

---
+++ vue练习/2/main.js
@@ -0,0 +1,23 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        firstName:'aa',
+        lastName:'ww',
+        fullName:''
+    },
+    computed: {
+        getFullName: function () {
+            let{firstName,lastName} = this
+            this.fullName = firstName+lastName
+            return this.fullName
+        }
+    },
+    watch: {
+        firstName: function (val) {
+            this.fullName = val + ' ' + this.lastName
+        },
+        lastName: function (val) {
+            this.fullName = this.firstName + ' ' + val
+        }
+    }
+})
