51ea5f9ddceb1f7ff1effec7de59a6af0bb3d9a7
dclangx <dc_langx@126.com>
2019-06-06 19:01:15 +0800

    图片上传

--- NodeJs + VueJs (Express + ElementUI) 全栈开发王者荣耀手机端官网和管理后台/admin/src/views/ItemEdit.vue
+++ NodeJs + VueJs (Express + ElementUI) 全栈开发王者荣耀手机端官网和管理后台/admin/src/views/ItemEdit.vue
@@ -11,7 +11,15 @@
                 <el-input v-model="ruleForm.name"></el-input>
             </el-form-item>
             <el-form-item label="图标" prop="icon">
-                <el-input v-model="ruleForm.icon"></el-input>
+                <el-upload
+                    class="avatar-uploader"
+                    :action="$http.defaults.baseURL+'/upload'"
+                    :show-file-list="false"
+                    :on-success="uploadSuccess"
+                >
+                    <img v-if="ruleForm.icon" :src="ruleForm.icon" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                </el-upload>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" native-type="submit">保存</el-button>
@@ -50,6 +58,9 @@ export default {
         async fetch() {
             const res = await this.$http.get(`rest/items/${this.id}`);
             this.ruleForm = res.data;
+        },
+        uploadSuccess(res,file) {
+            this.$set(this.ruleForm,'icon',res.url)
         }
     },
     created() {
@@ -59,4 +70,27 @@ export default {
 </script>
 
 <style>
+.avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+    border-color: #409eff;
+}
+.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+}
+.avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+}
 </style>
