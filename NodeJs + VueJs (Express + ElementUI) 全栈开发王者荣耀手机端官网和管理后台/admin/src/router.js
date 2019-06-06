f8851e5418c1fedcd0e820dc94ad3712f486a081
dclangx <dc_langx@126.com>
2019-06-06 15:14:30 +0800

    子分类完成

--- NodeJs + VueJs (Express + ElementUI) 全栈开发王者荣耀手机端官网和管理后台/admin/src/router.js
+++ NodeJs + VueJs (Express + ElementUI) 全栈开发王者荣耀手机端官网和管理后台/admin/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Main from './views/Main.vue'
 import CategoryEdit from './views/CategoryEdit.vue'
+import CategoryList from './views/CategoryList.vue'
 
 
 Vue.use(Router)
@@ -18,6 +19,13 @@ export default new Router({
       children:[{
         path: '/categories/create',
         component: CategoryEdit,
+      },{
+        path: '/categories/list',
+        component: CategoryList,
+      },{
+        path: '/categories/edit/:id',
+        component: CategoryEdit,
+        props:true
       }]
     },
     {
---
+++ NodeJs + VueJs (Express + ElementUI) 全栈开发王者荣耀手机端官网和管理后台/server/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+app.use(require('cors')())
+app.use(express.json())
+require('./plugins/db.js')(app)
+require('./routes/admin')(app)
+app.listen(3000,()=>{
+    console.log('app listen on port 3000')
+})
