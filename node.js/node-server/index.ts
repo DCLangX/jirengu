b3eca466462eb2c4decc7556a3e98cd31ec6f965
DC_LangX <dc_langx@126.com>
2020-05-08 11:46:31 +0800

    update ts-node

--- node.js/node-server/index.ts
+++ node.js/node-server/index.ts
@@ -1,11 +1,23 @@
-import * as http from 'http';
-import { IncomingMessage, ServerResponse } from 'http';
+import * as http from 'http'
+import * as fs from 'fs'
+import * as p from 'path'
+import * as url from 'url'
+import { IncomingMessage, ServerResponse } from 'http'
 const server = http.createServer()
+const publicDir = p.resolve(__dirname, 'public')
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   console.log('有人请求了')
-  console.log(request.url)
-  console.log('有人请求了')
-  response.end('hi')
+  const { method, url: path, headers } = request
+  // switch(url){
+  //   case '/index.html':
+  console.log(url.parse(path))
+  const urlObj = url.parse(path)
+  fs.readFile(p.resolve(publicDir, urlObj.pathname.slice(1)), (err, buffer) => {
+    if (err) throw err
+    response.end(buffer.toString())
+    console.log('publicDir', buffer.toString())
+  })
+  // }
 })
 
 server.listen(8888)
